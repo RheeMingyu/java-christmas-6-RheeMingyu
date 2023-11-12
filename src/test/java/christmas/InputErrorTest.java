22be183f5a047e33448baa4d4960a6538b09e4dd
RheeMingyu <stone262@naver.com>
2023-11-12 14:22:29 +0900

    test: 입력시 예외처리 가능한 요소 및 불가능한 요소 테스트,입력값 형변환 테스트 완료

--- src/test/java/christmas/InputErrorTest.java
+++ src/test/java/christmas/InputErrorTest.java
@@ -2,15 +2,20 @@ package christmas;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 
+import static constants.Constants.CREATE;
+
 public class InputErrorTest extends NsTest {
 
-	@DisplayName("입력값 수용의 유연성 테스트")
+	@DisplayName("입력값 수용의 유연성 테스트.")
 	@Test
 	void flexibility() {
 		 assertSimpleTest(() -> {
@@ -20,6 +25,45 @@ public class InputErrorTest extends NsTest {
 	            	"티본스테이크 1개",
 	            	"해산물파스타 1개",
 	            	"레드와인 1개"
+	            );
+	     });
+	}
+	
+	@DisplayName("12월에 없는 날짜 입력 시 오류 발생.")
+	@ValueSource(strings = {"0","32","","date"})
+	@ParameterizedTest
+	void noneOfDecember(String input) {
+		assertThatThrownBy(() -> CREATE.valid().dateValidator(input))
+	    						.isInstanceOf(IllegalArgumentException.class);
+	}
+	
+	@DisplayName("메뉴 입력조건에 부합하지 않는 형식 입력 시 오류 발생.")
+	@ValueSource(strings = {
+		"새우버거-2", //부존재 메뉴
+		"타파스-0", //0개 이하의 메뉴
+		"제로콜라:3", //형식 파괴("-"이외의 기호사용)
+		"티본스테이크-1.샴페인-1", //형식파괴(","이외의 기호사용)
+		"", //형식파괴(메뉴 미입력)
+		"티본스테이크-1,,샴페인-1", //형식파괴(무의미한 입력)
+		"타파스-one", //형식파괴(입력 인분 수 오류)
+		"타파스-1,제로콜라-1,타파스-1" //중복 메뉴
+	})
+	@ParameterizedTest
+	void unfittedFormat(String input) {
+		assertThatThrownBy(() -> CREATE.valid().orderValidator(input))
+								.isInstanceOf(IllegalArgumentException.class);
+	}
+	
+	@DisplayName("예외 처리 후 반복 입력 가능.")
+	@Test
+	void repeatable() {
+		 assertSimpleTest(() -> {
+			 run("a","1","티본스테이크-a","1","티본스테이크-1");
+	            assertThat(output()).contains(
+	            	"[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
+	            	"[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+	            	"12월 1일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!",
+	            	"티본스테이크 1개"
 	            );
 	     });
 	}
---
+++ src/test/java/christmas/StatTest.java
@@ -0,0 +1,47 @@
+package christmas;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import data.EventData;
+import features.Statistics;
+
+import static constants.Constants.CREATE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StatTest {
+	
+	private int testdate=25; //디데이 3400 평일 8092 특별 1000 증정 25000 총 37492
+	private Map<String, Integer> testorders=new HashMap<>() {
+		{
+			put("타파스",1); //애피타이저 5500
+			put("티본스테이크",2); //메인 110000
+			put("레드와인",3); //음료 180000
+			put("아이스크림",4); //디저트 20000 총합 315500
+		}
+	};
+	private EventData testdto=CREATE.dto();
+	
+	private Statistics teststat() {
+		testdto.setVisitDate(testdate);
+		testdto.setOrders(testorders);
+		
+		return CREATE.stat(testdto);
+	}
+
+	@DisplayName("할인 전 총주문 금액 계산 가능.")
+	@Test
+	void originalCost() {		
+		assertEquals(315500, teststat().getTotalCostOrigin());
+	}
+	
+	@DisplayName("증정 이벤트 당첨 여부 계산 가능.")
+	@Test
+	void presentation() {
+		assertTrue(teststat().deserveGift());
+	}
+}
