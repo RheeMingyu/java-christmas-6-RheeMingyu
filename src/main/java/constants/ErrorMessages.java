5deff2da0bb1f376d34633bfe0000522a97b9023
RheeMingyu <stone262@naver.com>
2023-11-13 19:58:31 +0900

    style: 불필요한 주석제거

--- src/main/java/constants/ErrorMessages.java
+++ src/main/java/constants/ErrorMessages.java
@@ -1,9 +1,6 @@
 package constants;
 
 public enum ErrorMessages {
-	
-	//private final static String INVALID_DATE_PROVIDED=;
-	//private final static String INVALID_MENU_PROVIDED=;
 
 	INVALID_DATE_OUTPUT("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
 	INVALID_MENU_OUTPUT("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
